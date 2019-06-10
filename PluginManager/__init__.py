91cea32f2e1ae15ec4f3e3f5ea986e42436dec44
mariosmsk <mariosmsk@gmail.com>
2019-06-10 17:26:17 +0300

    add message when install plugin

--- PluginManager/__init__.py
+++ PluginManager/__init__.py
@@ -131,7 +131,7 @@ def update_metadata():
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; font-size:8pt; font-weight:600; color:#000000; background-color:#ffffff;\">Plugin Name:</span><span style=\" font-family:\'Courier New\'; font-size:8pt; color:#000000; background-color:#ffffff;\"> "+ plugin_name_sel +" </span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; font-size:8pt; font-weight:600; color:#000000; background-color:#ffffff;\">Plugin Name:</span><span style=\" font-family:\'Courier New\'; font-size:8pt; color:#000000; background-color:#ffffff;\">"+ plugin_name_sel +" </span></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; font-size:8pt; font-weight:600; color:#000000; background-color:#ffffff;\">Version    :</span><span style=\" font-family:\'Courier New\'; font-size:8pt; color:#000000; background-color:#ffffff;\">" + version +"</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:\'Courier New\'; font-size:8pt; color:#000000;\"><br /></p>\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Courier New\'; font-size:8pt; font-weight:600; color:#000000; background-color:#ffffff;\">Description:</span><span style=\" font-family:\'Courier New\'; font-size:8pt; color:#000000; background-color:#ffffff;\">" + desc +"</span></p>\n"
@@ -182,6 +182,7 @@ def btn_installed():
 
     refresh_menuPlugin(iface)
 
+    QMessageBox.information(None, plugin_name_sel, 'Plugin installed successfully.', QMessageBox.Ok)
 
 def refresh_menuPlugin(session):
 
